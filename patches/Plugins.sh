443e460218fff504cbbf1d1cfbc587aff8d52c1c
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-03-09 21:36:33 +0800

    Update Plugins.sh

--- patches/Plugins.sh
+++ patches/Plugins.sh
@@ -19,12 +19,12 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
-export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+export CORE_TUN=https://github.com/vernesong/OpenClash/tree/core/dev/premium
+export CORE_DEV=https://github.com/vernesong/OpenClash/tree/core/dev/dev
+export CORE_MATE=https://github.com/vernesong/OpenClash/tree/core/dev/meta
 
-#export CORE_TYPE=$(echo redmiax6000 | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-export CORE_TYPE=echo "arm64"
+export CORE_TYPE=$(echo redmiax6000 | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+
 export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
 export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
@@ -37,10 +37,8 @@ curl -sfL -o ./Country.mmdb $GEO_MMDB
 curl -sfL -o ./GeoSite.dat $GEO_SITE
 curl -sfL -o ./GeoIP.dat $GEO_IP
 
-#mkdir ./core && cd ./core
-mkdir ./core
-cd $GITHUB_WORKSPACE/openwrt/package/feeds/luci/OpenClash/luci-app-openclash/root/etc/openclash/core
-ls
+mkdir ./core && cd ./core
+
 curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
 gzip -d ./tun.gz && mv ./tun ./clash_tun
 
