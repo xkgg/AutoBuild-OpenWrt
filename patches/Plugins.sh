c85acdb109b4e3b4b35252d6cd260c57c40b5271
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-03-10 22:58:30 +0800

    Update Plugins.sh

--- patches/Plugins.sh
+++ patches/Plugins.sh
@@ -20,7 +20,7 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
 export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-export CORE_TUN=https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-$TUN_VER.gz
+export CORE_TUN=https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64
 export CORE_DEV=https:/raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz
 export CORE_MATE=https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
 
@@ -40,20 +40,19 @@ curl -sfL -o ./GeoIP.dat $GEO_IP
 
 mkdir -p ./core && cd ./core
 
-curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-#gzip -d ./tun.gz && mv ./tun ./clash_tun
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$TUN_VER".gz
+gzip -d ./tun.gz && mv ./tun ./clash_tun
+chmod 0755 ./clash_tun
 
 curl -sfL -o ./meta.tar.gz "$CORE_MATE"
-# 4. 下载核心文件（带重试机制）
-
-ls
-#tar -zxf ./meta.tar.gz && mv -f clash ./clash_meta
 tar -zxf ./meta.tar.gz && mv -f clash ./clash_meta
 chmod 0755 ./clash_meta
-#chmod 0755 ./clash_meta
+
+
 echo "OpenClash core has been successfully integrated."
 
-curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-#tar -zxf ./dev.tar.gz
+curl -sfL -o ./dev.tar.gz "$CORE_DEV"
+tar -zxf ./dev.tar.gz
+find . -type f -exec chmod 0755 {} \;
 
 chmod +x ./clash* ; rm -rf ./*.gz
