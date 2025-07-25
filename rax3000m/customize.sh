d2b597f914a49e43d2fe50b9768115170be8a2f2
昕风挽笑 <xkgg368@gmail.com>
2025-07-25 21:01:48 +0800

    更新 rax3000M

---
+++ rax3000m/customize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify default TZ 时区
+sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
+sed -i '315a set system.@system[-1].zonename=\'Asia/Shanghai\'' openwrt/package/base-files/files/bin/config_generate
+
+
+sed -i '15a option sequential_ip	1' openwrt/package/network/services/dnsmasq/files/dhcp.conf
+
+#2. Clear the login password
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/base-files/files/etc/shadows
+
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf
+#4. 修改wifi名字
+#sed -i "s#ssid='[^']*'#ssid='Redmi_5A21'#g" openwrt/package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
