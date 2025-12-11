54810801023a1470fd88142299328b46b9dc38a3
昕风挽笑 <38105508+xkgg@users.noreply.github.com>
2025-12-11 20:14:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,3 +16,8 @@ sed -i 's/192.168.1.1/192.168.31.1/g' openwrt/package/base-files/files/bin/confi
 #4. 修改wifi名字
 sed -i "s#ssid='[^']*'#ssid='Redmi_5A21'#g" openwrt/package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
+#5. 修改顺序分配IP地址
+sed -i '15a option sequential_ip	1' openwrt/package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/option start 	100/option start 	2/g' openwrt/package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/option limit	150/option limit	255/g' openwrt/package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/option leasetime	12h/option leasetime	7d/g' openwrt/package/network/services/dnsmasq/files/dhcp.conf
